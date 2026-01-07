298a87ef4cce99f167740fa437b9b5ef7a2028ae
tkipngeno <tkipngeno@LTTECHMAC982.local>
2026-01-08 00:00:23 +0300

    new url

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:5000/api';
+const API_BASE_URL = 'https://ugandavote-backend.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
